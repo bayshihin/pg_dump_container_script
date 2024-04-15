64682cb1a1d30536c09f5f0759c38643962742b4
Kamil <149625264+bayshihin@users.noreply.github.com>
2024-04-16 00:42:58 +0500

    Create pg_dump.sh

---
+++ pg_dump.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Parameters for connecting to the PostgreSQL database
+PGHOST=""
+PGUSER=""
+PGPASSWORD=""
+DATABASE=""
+
+# Create a backup file name with the current date
+BACKUP_FILE="db_backup_$(date +'%Y%m%d').sql"
+
+# Path to save the backup
+cd /your/path/to/backup/dir/
+
+# Create a database backup
+docker exec <POSTGRESQL_CONTAINER_NAME> pg_dump -h $PGHOST -U $PGUSER -d $DATABASE > $BACKUP_FILE   # replace <POSTGRESQL_CONTAINER_NAME> with your posrgresql container name
+
+# Archiving a backup
+tar -czf $BACKUP_FILE.tar.gz $BACKUP_FILE
+
+# Deleting the original backup file
+rm $BACKUP_FILE
+
+# Push archive to remote repository (optional)
+cd /your/path/to/localrepo/with/backups
+git add .
+git commit -m "Added database backup"
+git push origin main
